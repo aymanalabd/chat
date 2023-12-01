b3f100df9ebd65d8fb78c0c2822c6d460f6cf976
aymanalabd <aymanalabd2000@gmail.com>
2023-12-01 20:50:40 +0200

    edit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import app from "./app.js";
 import "./database.js";
-import http from "http";
+import https from "https";
 import {Server} from "socket.io";
 import sockets from "./sockets.js";
 
-const server = http.createServer(app);
+const server = https.createServer(app);
 const io =new Server(server,{
 
     cors: {
